16b219ede9496dfd9a7b8df48c4ae9f173ed44b9
kayleey-khaleesy <basetsanakarabelo@gmail.com>
2024-11-28 18:56:04 +0200

    fixed JS weather data

--- src/index.js
+++ src/index.js
@@ -82,5 +82,10 @@ function dislplayForecast() {
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
+function searchCity(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 searchCity("Paris");
 dislplayForecast();
